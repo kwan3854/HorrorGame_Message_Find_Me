cc7df76173d8d78c7759e678334af5039ddced8d
kwan3854 <leekwanjung@gmail.com>
2022-08-04 01:06:52 +0900

    FEAT: 귀신 AI 수정
    더욱 잘 쫒아오게, 플레이어 감지시 색 변경 및 사운드 출력

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -5,26 +5,29 @@ using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
-    public NavMeshAgent navMeshAgent;
-    public float startWaitTime = 4;
-    public float timeToRotate = 2;
-    public float walkSpeed = 6;
-    public float runSpeed = 9;
+    private NavMeshAgent navMeshAgent;
+    [SerializeField] private AudioClip chasingSound;
+    [SerializeField] private AudioClip patrolSound;
+    [SerializeField] private float startWaitTime = 1;
+    [SerializeField] private float timeToRotate = 1;
+    [SerializeField] private float walkSpeed = 6;
+    [SerializeField] private float runSpeed = 9;
 
-    public float viewRadius = 15;
-    public float viewAngle = 90;
-    public LayerMask playerMask;
-    public LayerMask obstacleMask;
-    public float meshResolution = 1f;
-    public int edgeIterations = 4;
-    public float edgeDistance = 0.5f;
+    [SerializeField] private float viewRadius = 60;
+    [SerializeField] private float viewAngle = 360;
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private LayerMask obstacleMask;
+    [SerializeField] private float meshResolution = 1f;
+    [SerializeField] private int edgeIterations = 4;
+    [SerializeField] private float edgeDistance = 0.5f;
 
-    public Transform[] waypoints;
-    int m_CurrentWaypointIndex;
+    [SerializeField] private Transform[] waypoints;
+    private int m_CurrentWaypointIndex;
 
     Vector3 playerLastPosition = Vector3.zero;
     Vector3 m_PlayerPosition;
     GameObject player;
+    ParticleSystem particle;
 
     float m_WaitTime;
     float m_TimeToRotate;
@@ -38,6 +41,9 @@ public class AIController : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player");
         Debug.Assert(player != null, "Player not found");
+
+        particle = GetComponentInChildren<ParticleSystem>();
+        Debug.Assert(particle != null, "Particle not found");
 
         m_PlayerPosition = Vector3.zero;
         m_IsPatroling = true;
@@ -60,10 +66,21 @@ public class AIController : MonoBehaviour
 
         if (!m_IsPatroling)
         {
+            //playing chasing sound by starting the coroutine if not already playing
+            if (GetComponent<AudioSource>().clip != chasingSound)
+            {
+                StartCoroutine(PlayChasingSound());
+                particle.startColor = Color.blue;
+            }
             Chasing();
         }
         else
         {
+            if (!GetComponent<AudioSource>().isPlaying)
+            {
+                StartCoroutine(PlayPatrolingSound());
+                particle.startColor = Color.red;
+            }
             Patroling();
         }
     }
@@ -78,10 +95,18 @@ public class AIController : MonoBehaviour
             Move(runSpeed);
             navMeshAgent.SetDestination(m_PlayerPosition);
         }
+        else
+        {
+            //Caught Player
+            // play sound
+            // disapear
+            // game over
+        }
         if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
         {
+            Debug.Log("TESTESTESTTEST");
             if (m_WaitTime <= 0 && !m_CaughtPlayer &&
-            Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) >= 6f)
+            Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) >= 30f)
             {
                 m_IsPatroling = true;
                 m_IsPlayerNear = false;
@@ -89,11 +114,13 @@ public class AIController : MonoBehaviour
                 m_TimeToRotate = timeToRotate;
                 m_WaitTime = startWaitTime;
                 navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+                Debug.Log("ChasingOut");
             }
             else
             {
-                if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) >= 2.5f)
+                if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) >= 15f)
                 {
+                    Debug.Log("ChaseStop");
                     Stop();
                     m_WaitTime -= Time.deltaTime;
                 }
@@ -212,5 +239,25 @@ public class AIController : MonoBehaviour
         {
             m_PlayerPosition = player.transform.position;
         }
+    }
+
+    private IEnumerator PlayChasingSound()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = chasingSound;
+        audioSource.spatialBlend = 0;
+        audioSource.Play();
+        yield return new WaitForSeconds(chasingSound.length);
+        audioSource.Stop();
+    }
+
+    private IEnumerator PlayPatrolingSound()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = patrolSound;
+        audioSource.spatialBlend = 1;
+        audioSource.Play();
+        yield return new WaitForSeconds(patrolSound.length);
+        audioSource.Stop();
     }
 }
--- Assets/Scripts/ScencarioManager.cs
+++ Assets/Scripts/ScencarioManager.cs
@@ -32,18 +32,12 @@ public class ScencarioManager : MonoBehaviour
     // ========== Test Code ========== //
     void OnEnable()
     {
-        string[] diaglogue = { "데모 게임입니다. 데모용 기본 튜토리얼 대화창입니다. 화면을 마우스 클릭하여 다음 대화창으로 넘어갑니다.",
-            "w,a,s,d 키로 이동 합니다.","f키로 손전등을 켜고 끌 수 있습니다.", "e키로 물체와 상호작용 할 수 있습니다.",
-            "R 키로 휴대전화를 꺼내거나 넣을 수 있습니다.", "ESC 키로 뒤로가거나 게임을 일시정지 할 수 있습니다.",
-            "이제 안내를 마칩니다." };
-        StartCoroutine(SendMessage_Coroutine("데모 테스트 메시지입니다.", 2f, true));
-        StartCoroutine(SendDialogue_Coroutine(diaglogue, 5f));
+        StoryProceed(0);
     }
     // ========== Test Code ========== //
 
     private IEnumerator SendDialogue_Coroutine(string[] dialougeScript, float delaySec)
     {
-
         yield return new WaitForSeconds(delaySec);
         GameManager.Instance.OpenGameUI(dialougeUI);
         Diaglogue.Instance.StartDialogue(dialougeScript);
@@ -57,5 +51,56 @@ public class ScencarioManager : MonoBehaviour
             GameManager.Instance.OpenGameUI(phoneUI);
         }
         PhoneUI.Instance.MessageIncome(message);
+    }
+
+    public void StoryProceed(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                StartCoroutine(SendMessage_Coroutine("책상 서랍에 둔 물건을 빼앗기고 싶지 않으면 오늘 11시에 학교로 찾아와라.", 2f, true));
+                StartCoroutine(SendDialogue_Coroutine(new string[]
+                { "누가 이런 장난을 쳤는지는 모르겠지만...",
+                "일단, 책상 서랍에서 물건을 찾고 돌아가자."}
+                , 5f));
+                break;
+
+            case 1:
+                StartCoroutine(SendDialogue_Coroutine(new string[]
+                { "여기 분명 뒀는데?",
+                "왜 없지?"}
+                , 1f));
+                break;
+            case 2:
+                StartCoroutine(SendDialogue_Coroutine(new string[]
+                { "안녕, 시간 맞춰 학교에 왔구나.",
+                "와 줘서 고마운걸.",
+                "이제부터 내가 하는 말을 들어주길 바라.",
+                "뒤에 있는 사물함에서 쪽지를 찾고 거기 쓰여있는 대로 행동하면",
+                "네가 찾던 물건을 찾고 무사히 집으로 돌아갈 수 있을거야.",
+                "그럼, 행운이 있길-."}
+                , 1f));
+                break;
+
+            case 3:
+                StartCoroutine(SendDialogue_Coroutine(new string[]
+                { "뭐지? 누가 이런 장난을 치는 거야?",
+                "찾던 물건은 왜 없는거지?",
+                "하... 그냥 집에 갈까? 피곤하기도 하고...",
+                "'그 물건' 을 찾으려면 쪽지를 찾아야 한다잖아.",
+                "어떡하지?"}
+                , 1f));
+                break;
+
+            case 4:
+                // StartCoroutine(SelectionWindow_Coroutine(new string[] { "방송을 따라 쪽지를 찾는다.", "방송을 무시하고 집에 간다."});
+                break;
+
+            case 5:
+                StartCoroutine(SendDialogue_Coroutine(new string[]
+                { "왜 이런 일을 나에게 시키는 걸까?"}
+                , 1f));
+                break;
+        }
     }
 }
